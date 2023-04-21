0f4684f5d224137a4491b9cd6a4c26190e4f2614
DEV-NHATHN <ongthosongtu@gmail.com>
2023-04-21 09:52:40 +0700

    commit 1

---
+++ Data/RoleName.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMVC.Data
+{
+    public class RoleName
+    {
+        public const string Administrator = "Administrator";
+        public const string Editor = "Editor";
+        public const string Member = "Member";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using App.Models;
+using Microsoft.AspNetCore.Mvc.Razor;
+using AppMVC.Data;
+using AppMVC.Models;
+using AppMVC.Services;
 
 namespace AppMVC
 {
@@ -12,6 +16,14 @@ namespace AppMVC
 
          // Add services to the container.
          var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+         builder.Services.AddDistributedMemoryCache();
+         builder.Services.AddSession(cfg =>
+         {
+            cfg.Cookie.Name = "AppMvc";
+            cfg.IdleTimeout = new TimeSpan(0, 30, 0);
+         });
+
          // Add services to the container.
          builder.Services.AddDbContext<AppDbContext>(options =>
          {
@@ -19,15 +31,73 @@ namespace AppMVC
          });
          builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-             .AddEntityFrameworkStores<AppDbContext>();
          builder.Services.AddControllersWithViews();
+         builder.Services.AddOptions();
          builder.Services.AddRazorPages();
+         builder.Services.AddTransient(typeof(ILogger<>), typeof(Logger<>));
+         builder.Services.Configure<RazorViewEngineOptions>(options =>
+         {
+            options.ViewLocationFormats.Add("/Custom/View/{1}/{0}" + RazorViewEngine.ViewExtension);
+         });
+         builder.Services.AddAuthorization(options =>
+         {
+            options.AddPolicy("ViewManageMenu", builder =>
+            {
+               builder.RequireAuthenticatedUser();
+               builder.RequireRole(RoleName.Administrator);
+               // builder.RequireClaim("permission", "manage.admin");
+            });
+         });
+
+         // builder.Services.AddTransient<IActionContextAccessor, ActionContextAccessor>();
+         // builder.Services.AddTransient<AdminSidebarService>();
+
+         // Dang ky Identity
+         builder.Services.AddIdentity<AppUser, IdentityRole>()
+                 .AddEntityFrameworkStores<AppDbContext>()
+                 .AddDefaultTokenProviders();
+
+         // Truy cập IdentityOptions
+         builder.Services.Configure<IdentityOptions>(options =>
+         {
+            // Thiết lập về Password
+            options.Password.RequireDigit = false; // Không bắt phải có số
+            options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+            options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+            options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+            options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+            options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+            // Cấu hình Lockout - khóa user
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+            options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lầ thì khóa
+            options.Lockout.AllowedForNewUsers = true;
+
+            // Cấu hình về User.
+            options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+
+            // Cấu hình đăng nhập.
+            options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+            options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+            options.SignIn.RequireConfirmedAccount = true;
+
+         });
+
+         builder.Services.ConfigureApplicationCookie(options =>
+         {
+            options.LoginPath = "/login/";
+            options.LogoutPath = "/logout/";
+            options.AccessDeniedPath = "/khongduoctruycap.html";
+         });
+
+         builder.Services.AddSingleton<IdentityErrorDescriber, AppIdentityErrorDescriber>();
 
          var app = builder.Build();
-
-         // Configure the HTTP request pipeline.
-         if (app.Environment.IsDevelopment())
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
          {
             app.UseMigrationsEndPoint();
          }
@@ -40,9 +110,8 @@ namespace AppMVC
 
          app.UseHttpsRedirection();
          app.UseStaticFiles();
-
+         app.UseSession();
          app.UseRouting();
-
          app.UseAuthentication();
          app.UseAuthorization();
 
@@ -50,6 +119,22 @@ namespace AppMVC
              name: "default",
              pattern: "{controller=Home}/{action=Index}/{id?}");
          app.MapRazorPages();
+
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapAreaControllerRoute(
+                name: "product",
+                areaName: "ProductManage",
+                pattern: "/{controller}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "first",
+                       pattern: "{url:regex(^view.*product$)}/{id:range(1,5)}",
+                       defaults: new { controller = "First", action = "ViewProduct" });
+            endpoints.MapControllerRoute(
+                       name: "default",
+                       pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+         });
 
          app.Run();
       }
